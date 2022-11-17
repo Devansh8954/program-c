c55874017e83dd03e1e662a1bdc8430bd57ce3e6
Devansh <devansh8954@gmail.com>
2022-11-17 21:41:05 +0530

    added queue using linkedlist

--- Queue_using_Linkedlist.c
+++ Queue_using_Linkedlist.c
@@ -1,93 +1,69 @@
 #include<stdio.h>
-#include<stdlib.h>
 void enqueue();
 void dequeue();
 void display();
 
-struct Node {
+struct Node
+{
    int data;
    struct Node *next;
-}* front= NULL,*rear=NULL,*tmp;
+}*front = NULL,*rear = NULL;
 
-void main() {
-   int ch, val,n;
-   xy:
-   printf("\n 1. Enqueue in queue \n");
-   printf("\n 2. Dequeue in stack \n");
-   printf("\n 3. Display \n");
-   printf("\n Enter your choice \n");
-   scanf("%d",&ch);
-      switch(ch) {
-         case 1: {
-            enqueue();
-            break;
-         }
-         case 2: {
-            dequeue();
-            break;
-         }
-         case 3: {
-            display();
-            break;
-         }
-         default: 
-         printf("\n Invalid choice \n");
-         break;
+void main()
+{
+   int choice, value;
+   printf("\n:: Queue Implementation using Linked List ::\n");
+   while(1){
+      printf("\n****** MENU ******\n");
+      printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
+      printf("Enter your choice: ");
+      scanf("%d",&choice);
+      switch(choice){
+	 case 1: printf("Enter the value to be insert: ");
+		 scanf("%d", &value);
+		 enqueue(value);
+		 break;
+	 case 2: dequeue(); break;
+	 case 3: display(); break;
+	 case 4: exit(0); 
+	 default: printf("\n Invalid choice \n");
       }
-    printf("\n Enter 1 to continue \n");
-    scanf("%d",&n);
-    if (n==1)
-    {
-        goto xy;
-    }
-}
-
-void enqueue() {
-    int val;
-    printf("\n Enter value to be pushed \n");
-    scanf("%d",&val);
-    if (rear == NULL) {
-    rear= (struct Node*) malloc(sizeof(struct Node));
-    rear->data = val;
-    rear->next = NULL;
-    front=rear;
-    }
-    else {
-      tmp=(struct Node *)malloc(sizeof(struct Node));
-      rear->next = tmp;
-      tmp->data = val;
-      tmp->next = NULL;
-      rear = tmp;
-    }
-}
-void dequeue() {
-   if(front == NULL)
-   printf("\n Queue Underflow \n");
-    if (tmp->next != NULL) {
-      tmp = tmp->next;
-      printf("\n Element deleted from queue is : %d \n",front->data);
-      free(front);
-      front = tmp;
-     } 
-    else {
-      printf("\n Element deleted from queue is : %d \n",front->data);
-      free(front);
-      front = NULL;
-      rear = NULL;
    }
 }
-void display() {
-   tmp = front;
-   if((tmp == NULL)) 
-   printf("\n Queue is empty \n");
-
-   else {
-      printf("\n Queue elements are: \n");
-      while(tmp != rear) {
-        printf("\n %d",tmp->data);
-        tmp = tmp->next;
-    }
-    printf("\n %d",tmp->data);
-    tmp = tmp->next;
+void enqueue(int value)
+{
+   struct Node *newNode;
+   newNode = (struct Node*)malloc(sizeof(struct Node));
+   newNode->data = value;
+   newNode -> next = NULL;
+   if(front == NULL)
+      front = rear = newNode;
+   else{
+      rear -> next = newNode;
+      rear = newNode;
+   }
+}
+void dequeue()
+{
+   if(front == NULL)
+      printf("\nQueue is Empty\n");
+   else{
+      struct Node *temp = front;
+      front = front -> next;
+      printf("\nDeleted element: %d\n", temp->data);
+      free(temp);
+   }
+}
+void display()
+{
+   if(front == NULL)
+      printf("\nQueue is Empty\n");
+   else{
+      struct Node *temp = front;
+      while(temp->next != NULL){
+	 printf("%d",temp->data);
+	 temp = temp -> next;
+      }
+      printf("%d\n",temp->data);
    }
 }
